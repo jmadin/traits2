d3108f9ba67b6c6be763b650e64053c54d0c6d17
Joshua Madin <jmadin@hawaii.edu>
2023-01-23 20:55:48 -1000

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,8 +16,3 @@ Precisiontype.create!([
   {user_id: 1, precision_type_name: "Standard deviation", precision_type_description: "", has_range: false},
   {user_id: 1, precision_type_name: "Range", precision_type_description: "The minimum and maximum values measurements.", has_range: true}
 ])
-
-Valuetype.create!([
-  {user_id: 1, value_type_name: "Raw value", value_type_description: "Value as measured by observer.", has_precision: false},
-  {user_id: 1, value_type_name: "Mean", value_type_description: "The mean of raw values as measured by observer. Allows for precision.", has_precision: true}
-])
