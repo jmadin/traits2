8060c227baeef7b917d70a6827db864a1276490f
Joshua Madin <jmadin@hawaii.edu>
2023-01-24 06:46:30 +0000

    huh

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2016_06_01_021815) do
-
+ActiveRecord::Schema[7.0].define(version: 2016_06_01_021815) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,8 +21,8 @@ ActiveRecord::Schema.define(version: 2016_06_01_021815) do
     t.boolean "resolved"
     t.text "resolved_description"
     t.integer "resolved_user"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.index ["observation_id"], name: "index_issues_on_observation_id"
     t.index ["user_id"], name: "index_issues_on_user_id"
   end
@@ -34,8 +33,8 @@ ActiveRecord::Schema.define(version: 2016_06_01_021815) do
     t.decimal "latitude"
     t.decimal "longitude"
     t.text "location_description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.boolean "approved"
     t.index ["user_id"], name: "index_locations_on_user_id"
   end
@@ -53,8 +52,8 @@ ActiveRecord::Schema.define(version: 2016_06_01_021815) do
     t.text "precision_upper"
     t.text "replicates"
     t.text "measurement_description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.index ["methodology_id"], name: "index_measurements_on_methodology_id"
     t.index ["observation_id"], name: "index_measurements_on_observation_id"
     t.index ["precisiontype_id"], name: "index_measurements_on_precisiontype_id"
@@ -69,8 +68,8 @@ ActiveRecord::Schema.define(version: 2016_06_01_021815) do
     t.string "methodology_name"
     t.text "methodology_description"
     t.boolean "approved"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.index ["user_id"], name: "index_methodologies_on_user_id"
   end
 
@@ -87,8 +86,8 @@ ActiveRecord::Schema.define(version: 2016_06_01_021815) do
     t.integer "resource_secondary_id"
     t.boolean "access"
     t.boolean "approved"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.index ["location_id"], name: "index_observations_on_location_id"
     t.index ["resource_id"], name: "index_observations_on_resource_id"
     t.index ["resource_secondary_id"], name: "index_observations_on_resource_secondary_id"
@@ -101,19 +100,19 @@ ActiveRecord::Schema.define(version: 2016_06_01_021815) do
     t.string "precision_type_name"
     t.text "precision_type_description"
     t.boolean "has_range"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["user_id"], name: "index_precisiontypes_on_user_id"
   end
 
   create_table "releases", force: :cascade do |t|
     t.integer "user_id"
     t.string "release_code"
-    t.datetime "release_date"
+    t.datetime "release_date", precision: nil
     t.string "release_link"
     t.text "release_description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["user_id"], name: "index_releases_on_user_id"
   end
 
@@ -128,8 +127,8 @@ ActiveRecord::Schema.define(version: 2016_06_01_021815) do
     t.text "volume_pages"
     t.text "resource_description"
     t.boolean "approved"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.index ["user_id"], name: "index_resources_on_user_id"
   end
 
@@ -139,8 +138,8 @@ ActiveRecord::Schema.define(version: 2016_06_01_021815) do
     t.text "specie_description"
     t.string "aphia_id"
     t.boolean "approved"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.index ["user_id"], name: "index_species_on_user_id"
   end
 
@@ -151,8 +150,8 @@ ActiveRecord::Schema.define(version: 2016_06_01_021815) do
     t.string "standard_class"
     t.string "standard_description"
     t.boolean "approved"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.index ["user_id"], name: "index_standards_on_user_id"
   end
 
@@ -161,8 +160,8 @@ ActiveRecord::Schema.define(version: 2016_06_01_021815) do
     t.string "synonym_name"
     t.text "synonym_description"
     t.boolean "approved"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.index ["specie_id"], name: "index_synonyms_on_specie_id"
   end
 
@@ -171,8 +170,8 @@ ActiveRecord::Schema.define(version: 2016_06_01_021815) do
     t.string "class_name"
     t.text "class_description"
     t.boolean "contextual"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.index ["user_id"], name: "index_traitclasses_on_user_id"
   end
 
@@ -186,8 +185,8 @@ ActiveRecord::Schema.define(version: 2016_06_01_021815) do
     t.boolean "approved"
     t.boolean "released"
     t.boolean "hide"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.index ["standard_id"], name: "index_traits_on_standard_id"
     t.index ["traitclass_id"], name: "index_traits_on_traitclass_id"
     t.index ["user_id"], name: "index_traits_on_user_id"
@@ -202,8 +201,8 @@ ActiveRecord::Schema.define(version: 2016_06_01_021815) do
     t.string "value_name"
     t.integer "trait_id"
     t.text "value_description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.index ["trait_id"], name: "index_traitvalues_on_trait_id"
   end
 
@@ -212,8 +211,8 @@ ActiveRecord::Schema.define(version: 2016_06_01_021815) do
     t.string "last_name"
     t.string "institution"
     t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
@@ -221,10 +220,10 @@ ActiveRecord::Schema.define(version: 2016_06_01_021815) do
     t.boolean "editor", default: false
     t.string "activation_digest"
     t.boolean "activated", default: false
-    t.datetime "activated_at"
+    t.datetime "activated_at", precision: nil
     t.string "reset_digest"
-    t.datetime "reset_sent_at"
-    t.datetime "last_seen_at"
+    t.datetime "reset_sent_at", precision: nil
+    t.datetime "last_seen_at", precision: nil
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["remember_digest"], name: "index_users_on_remember_digest"
   end
@@ -234,8 +233,8 @@ ActiveRecord::Schema.define(version: 2016_06_01_021815) do
     t.string "value_type_name"
     t.text "value_type_description"
     t.boolean "has_precision"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["user_id"], name: "index_valuetypes_on_user_id"
   end
 
@@ -245,7 +244,7 @@ ActiveRecord::Schema.define(version: 2016_06_01_021815) do
     t.string "event", limit: 255, null: false
     t.string "whodunnit", limit: 255
     t.text "object"
-    t.datetime "created_at"
+    t.datetime "created_at", precision: nil
     t.text "object_changes"
     t.string "ip", limit: 255
     t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
